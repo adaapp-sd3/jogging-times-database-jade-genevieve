f8422c6e3798d8c4c3092564186054aa4ad1e6a7
Alex Cline <AlexCline@ada.ac.uk>
2019-01-16 13:09:16 +0000

    changing to use mysql, config needs to be updated

--- database.js
+++ database.js
@@ -1,8 +1,23 @@
-// load the sqlite library
-var sqlite = require('better-sqlite3')
+//load the mysql library
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'example.org',
+  user     : 'bob',
+  password : 'secret'
+});
 
-// open a database
-var db = sqlite('./database.sqlite')
+//create connection 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+ 
+  console.log('connected as id ' + connection.threadId);
+});
 
 // export the database so we can use it elsewhere
 module.exports = db
+
+
+
